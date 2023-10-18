e45baebb2e2100b6bc35b01356793f298dd18118
Joan BONDRI <jbondri@bess-f2r5s13.clusters.42paris.fr>
2023-10-18 18:24:01 +0200

    implement basic functions

---
+++ kernel/libc/inc/cdefs.h
@@ -0,0 +1,6 @@
+#ifndef CDEFS_H
+#define CDEFS_H 1
+ 
+#define __myos_libc 1
+ 
+#endif
--- kernel/src/utils/strlen.c
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-
-size_t strlen(const char *s)
-{
-	const char *s2 = s;
-	while (*s2)
-		++s2;
-	return s2 - s;
-}
