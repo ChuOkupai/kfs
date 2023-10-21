fe58797a1e9ef9188d483abd4fac725835dfe4c0
Adrien Soursou <asoursou@student.42.fr>
2023-10-21 18:31:08 +0200

    Added workspaces support

---
+++ kernel/src/tty/tty_switch_workspace.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <tty.h>
+
+static void workspace_switch(size_t next) {
+	t_workspace *w = tty_get_current_workspace();
+	t_workspace *next_w = g_term->workspaces + next;
+
+	// Save the current workspace
+	memcpy(w->buf, VGA_MEMORY, VGA_BUFSIZE * sizeof(t_vga_entry));
+	// Switch to the next workspace
+	memcpy(VGA_MEMORY, next_w->buf, VGA_BUFSIZE * sizeof(t_vga_entry));
+	g_term->current_workspace = next;
+}
+
+void tty_prev_workspace() {
+	workspace_switch((g_term->current_workspace + MAX_WORKSPACES - 1) % MAX_WORKSPACES);
+}
+
+void tty_next_workspace() {
+	workspace_switch((g_term->current_workspace + 1) % MAX_WORKSPACES);
+}
