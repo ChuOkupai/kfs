5286d3d178129dae933e235c59f51a018f6ce673
Joan <51259134+joanBondri@users.noreply.github.com>
2023-10-29 16:45:16 +0100

    18 fix backspace behavior - Backspace is good (#32)
    Fix backspace behavior

--- kernel/src/tty/tty_init.c
+++ kernel/src/tty/tty_init.c
@@ -9,7 +9,7 @@ static inline void workspace_init(t_workspace *s) {
 	s->column = 0;
 	s->color = vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
 	for (size_t i = 0; i < VGA_BUFSIZE; ++i)
-		s->buf[i] = vga_entry(' ', s->color);
+		s->buf[i] = vga_entry('\0', s->color);
 }
 
 void tty_init() {
--- kernel/src/tty/tty_putc.c
+++ kernel/src/tty/tty_putc.c
@@ -7,6 +7,23 @@ static inline void workspace_putc(t_workspace *s, char c) {
 			s->row = 0;
 		return;
 	}
+	else if (c == '\a')
+	{
+		t_vga_entry	blank = vga_entry('\0', s->color);
+		if (s->column != 0)
+			--s->column;
+		else
+		{
+			if (s->row == 0)
+				return ;
+			--s->row;
+			s->column = VGA_WIDTH;
+			while (s->column != 0 && !vga_entry_cmp(VGA_MEMORY[s->row * VGA_WIDTH + s->column], blank))
+				s->column--;
+		}
+		vga_put('\0', s->color, s->column, s->row);
+		return ;
+	}
 	vga_put(c, s->color, s->column, s->row);
 	if (++s->column == VGA_WIDTH) {
 		s->column = 0;
