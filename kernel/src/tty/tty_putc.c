4bfe70a6465fc1fe8feeef963c387ed0dfad20ad
Joan BONDRI <jbondri@student.42.fr>
2023-11-01 12:07:12 +0100

    move

--- kernel/src/tty/tty_putc.c
+++ kernel/src/tty/tty_putc.c
@@ -1,39 +1,14 @@
+#include <vga.h>
+#include <vgaline.h>
 #include <tty.h>
 
 static inline void workspace_putc(t_workspace *s, char c) {
-	t_vga_color focus_color = vga_entry_color(VGA_COLOR_BLACK, VGA_COLOR_WHITE);
-	if (c == '\a')
-	{
-		vga_put_color(s->color, s->column, s->row);
-		t_vga_entry	blank = vga_entry('\0', s->color);
-		if (s->column != 0)
-			--s->column;
-		else if (s->row != 0)
-		{
-			--s->row;
-			s->column = VGA_WIDTH;
-			while (s->column != 0
-			&& (!vga_entry_cmp(VGA_MEMORY[s->row * VGA_WIDTH + s->column], blank)
-			|| !vga_entry_cmp(VGA_MEMORY[s->row * VGA_WIDTH + s->column], '\0' | focus_color << 8)))
-				s->column--;
-		}
-		vga_put('\0', focus_color, s->column, s->row);
-		return ;
-	}
+	static struct s_list_vga_line	*start = alloc_next_line();
+	static t_vga_entry				*on_focus = start->line + 0;
+	t_vga_entry_color				focus_color = VGA_COLOR_BLACK | VGA_COLOR_WHITE << 4;
 
-	vga_put(c, s->color, s->column, s->row);
-	if (c == '\n') {
-		s->column = 0;
-		if (++s->row == VGA_HEIGHT)
-			s->row = 0;
-		return;
-	}
-	if (++s->column == VGA_WIDTH) {
-		s->column = 0;
-		if (++s->row == VGA_HEIGHT)
-			s->row = 0;
-	}
-	vga_put_color(focus_color, s->column, s->row);
+	*on_focus |= focus_color << 8;
+	
 }
 
 void tty_putc(char c) {
