fc719806d3511f83bacefea0b23161248da4e27e
Adrien Soursou <asoursou@student.42.fr>
2024-05-14 14:56:43 +0200

    Fix erase current line

--- kernel/src/tty/tty_erase_line.c
+++ kernel/src/tty/tty_erase_line.c
@@ -2,6 +2,7 @@
 
 void tty_erase_line() {
 	t_workspace *w = tty_current_workspace();
-	size_t start = w->rl_cursor - w->rl_cursor % TTY_WIDTH;
-	vga_fill(0, TTY_DEFAULT_COLOR, start, TTY_BUFSIZE - start);
+	tty_move_end_of_line();
+	vga_fill(0, TTY_DEFAULT_COLOR, w->rl_start, TTY_BUFSIZE - w->rl_start);
+	w->rl_cursor = w->rl_start;
 }
